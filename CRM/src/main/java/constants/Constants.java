7877a32ed9683346606bbb1f90d1b742ed917a05
user <user@DESKTOP-UMO1HLL>
2023-12-08 21:11:27 +0530

    new

---
+++ CRM/src/main/java/constants/Constants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Constants
+{
+	public static final String screenShot_path= System.getProperty("user.dir") + "\\Screenshots\\";
+	public static final String propertyConfig_File=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";////System.getProperty("user.dir")-we will get the project path
+	public static final String testData=System.getProperty("user.dir")+"\\src\\test\\resources\\testdata\\crmdata.xlsx";
+}
